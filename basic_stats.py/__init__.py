cf54370c6497c89ab1e30d1515dcd3bcea09ba42
AllisonKaht <allisonkaht@uri.edu>
2023-11-21 17:47:05 -0500

    Update Info

---
+++ basic_stats.py/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+basic_stats Package for Python
+    Version 1.0 Nov. 21, 2023
+    Author:  Allison Kaht
+    Email: allisonkaht@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .Basic_stats_complete_GROUP.ipynb import *
+
+
+__all__ = ['sat_vp']
