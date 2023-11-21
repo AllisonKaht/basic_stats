8f93bee421bfa40f7451ad62aec78f9ad3688c50
AllisonKaht <allisonkaht@uri.edu>
2023-11-21 18:02:30 -0500

    setup edits

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "Basic_stats_complete_GROUP",
+    version = "0.1",
+    author = "Allison Kaht",
+    packages=['Basic_stats_complete_Group'],
+    install_requires=['numpy','scipy.io'],
+)
